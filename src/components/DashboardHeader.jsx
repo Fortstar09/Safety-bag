79600ea722100f69cccba7e71e195fc1430a00a2
Fortstar09 <ogunyafort@gmail.com>
2025-07-16 22:03:42 +0100

    hamburger menu

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,12 +1,112 @@
-import { Menu, Settings, UserRound } from "lucide-react";
+import {
+  ChartCandlestick,
+  CircleUserRound,
+  CopyCheck,
+  HandCoins,
+  Landmark,
+  Menu,
+  NotepadText,
+  Pickaxe,
+  Settings,
+  UserRound,
+  Wallet,
+} from "lucide-react";
 import { Button } from "./ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./ui/sheet";
 
 const DashboardHeader = () => {
   return (
     <nav className="px-5 fixed py-3 w-full border-b bg-white z-10">
       <div className="flex justify-between">
         <div className="flexCenter gap-2">
-          <Menu size={20} className="md:hidden"/>
+          <Sheet>
+            <SheetTrigger>
+              <Menu size={20} className="md:hidden" />
+            </SheetTrigger>
+            <SheetContent className="w-[250px] sm:w-[540px] px-0" side="left">
+              <SheetHeader>
+                <ul className="w-full h-full flex flex-col gap-0 items-start justify-start pt-5">
+                  <li className="hover:bg-[#15973F33] w-full rounded-md mb-2">
+                    <a
+                      href="/trading"
+                      className="text-base font-medium text-grey1  flex items-center gap-2 py-4 px-5 rounded-md transition-colors duration-200"
+                    >
+                      <ChartCandlestick />
+                      <span>Trading</span>
+                    </a>
+                  </li>
+                  <li className="hover:bg-[#15973F33] w-full rounded-md mb-2">
+                    <a
+                      href="/deposit"
+                      className="text-base font-medium text-grey1  flex items-center gap-2 py-4 px-5 rounded-md transition-colors duration-200"
+                    >
+                      <Landmark />
+                      <span>Deposits</span>
+                    </a>
+                  </li>
+                  <li className="hover:bg-[#15973F33] w-full rounded-md mb-2">
+                    <a
+                      href="/withdraw"
+                      className="text-base font-medium text-grey1  flex items-center gap-2 py-4 px-5 rounded-md transition-colors duration-200"
+                    >
+                      <HandCoins />
+                      <span>Withdrawals</span>
+                    </a>
+                  </li>
+                  <li className="hover:bg-[#15973F33] w-full rounded-md mb-2">
+                    <a
+                      href="/pricing"
+                      className="text-base font-medium text-grey1  flex items-center gap-2 py-4 px-5 rounded-md transition-colors duration-200"
+                    >
+                      <NotepadText />
+                      <span>Plans</span>
+                    </a>
+                  </li>
+                  <li className="hover:bg-[#15973F33] w-full rounded-md mb-2">
+                    <a
+                      href="/mine"
+                      className="text-base font-medium text-grey1  flex items-center gap-2 py-4 px-5 rounded-md transition-colors duration-200"
+                    >
+                      <Pickaxe />
+                      <span>Mining</span>
+                    </a>
+                  </li>
+                  <li className="hover:bg-[#15973F33] w-full rounded-md mb-2">
+                    <a
+                      href="/copy"
+                      className="text-base font-medium text-grey1  flex items-center gap-2 py-4 px-5 rounded-md transition-colors duration-200"
+                    >
+                      <CopyCheck />
+                      <span>Copy Trading</span>
+                    </a>
+                  </li>
+                  <li className="hover:bg-[#15973F33] w-full rounded-md mb-2">
+                    <a
+                      href="/hold"
+                      className="text-base font-medium text-grey1  flex items-center gap-2 py-4 px-5 rounded-md transition-colors duration-200"
+                    >
+                      <Wallet />
+                      <span>Holding</span>
+                    </a>
+                  </li>
+                  <li className="hover:bg-[#15973F33] w-full rounded-md mb-2">
+                    <a
+                      href="/account"
+                      className="text-base font-medium text-grey1  flex items-center gap-2 py-4 px-5 rounded-md transition-colors duration-200"
+                    >
+                      <CircleUserRound />
+                      <span>My Account</span>
+                    </a>
+                  </li>
+                </ul>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
           <img src="/icons/webLogo.svg" alt="logo" className="hidden md:flex" />
         </div>
 
